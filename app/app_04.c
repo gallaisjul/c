30001ee6e08c0816bff17acaaf707758bf9734da
Julien G <gallaisjulienlh76@gmail.com>
2022-08-22 21:00:04 +0200

    Overview of conditions n°2

--- app/app_04.c
+++ app/app_04.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
-    int number = 0;
-    
-    if(number == 0)
+    int salary_per_month = 0;	
+    int age = 0;
+    bool credit_agreement = false;
+
+    printf("How much do you have in your account? ");
+    scanf("%d", &salary_per_month);
+    printf("\n");
+    printf("How old are you? ");
+    scanf("%d", &age);
+    printf("\n");
+
+    if (salary_per_month > 1500 && age >= 18)
     {
-        printf("The number is 0.\n");
+        credit_agreement = true;
+	printf("We grant you $%d.\n", salary_per_month * 2);
+    }
+    else if (salary_per_month > 1000 && age >= 18)
+    {
+        printf("We can credit your accont with 100.\n");
     }
     else
     {
-        printf("The number is not 0.\n");
-    }    
-	
+        printf("We can't give you credit. Sorry\n");
+    }
+
     return 0;
 }
