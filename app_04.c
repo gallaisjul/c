a96e49dece0c7f80b6687fa8332be3bff0c4d601
Julien G <gallaisjulienlh76@gmail.com>
2022-08-22 16:53:14 +0200

    Overview of conditions n°1

---
+++ app_04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 0;
+    
+    if(number == 0)
+    {
+        printf("The number is 0.\n");
+    }
+    else
+    {
+        printf("The number is not 0.\n");
+    }    
+	
+    return 0;
+}
