bc49b5902902c3bb56cb8761f4fdfc45264c31d8
Julien G <gallaisjulienlh76@gmail.com>
2022-08-22 15:29:17 +0200

    Overview of opérators

---
+++ app_03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int nb1 = 5 + 10;
+    int nb2 = 3 * 3;
+    int nb3 = 8 / 2;
+    int nb4 = 10 % 8;
+
+    printf("%d\n", nb1);
+    printf("%d\n", nb4 * nb2);
+    printf("%d\n", nb3 + nb4);
+
+    
+    return 0;
+}
