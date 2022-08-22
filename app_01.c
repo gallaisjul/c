3da92dd4ceec2978abd0d026460098e0b95fc6ae
Julien G <gallaisjulienlh76@gmail.com>
2022-08-22 14:00:32 +0200

    Overview on register and volatile

--- app_01.c
+++ app_01.c
@@ -11,6 +11,8 @@ int main(void)
     float number = 124.23;
     int int_number = (int)number;
     const float PI = 3.14;
+    register int num_1 = 5;
+    volatile int num_2 = 10; 
 
     printf("%f\n", number);
     printf("%d\n", int_number);
