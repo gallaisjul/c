87bdfbb33674196d05d5b8ad5ed06c9b94bb63a6
Julien G <gallaisjulienlh76@gmail.com>
2022-08-24 14:36:14 +0200

    Les pointeurs, second prt

--- app/app_08.c
+++ app/app_08.c
@@ -2,17 +2,25 @@
 
 /*
  *  Pointeur : variable contenant l'adresse d'une autre variable
+ *  %d -> affiche une adresse de variable ou de pointeur 
+ *
+ *  [VARIABLES]
+ *      maVariable  : valeur de la variable
+ *      &maVariable : adresse de la variable 
+ *
+ *  [POINTEURS]
+ *      monPointeur  : adresse de la variable
+ *      *monPointeur : valeur de la variable pointée
+ *      &monPointeur : adresse du pointeur 
  */
 
-int inverse_number(int num_a, int num_b)
+void inverse_number(int *num_a,int *num_b)
 {
-    int temporary = 0;
+    int temporary = 0;//B
 
-    temporary = num_b;
-    num_b = num_a;
-    num_a = temporary;
-
-    return num_a;
+    temporary = *num_b;
+    *num_b = *num_a;
+    *num_a = temporary;
 }
 
 int main(void)
@@ -20,11 +28,8 @@ int main(void)
     int num_a = 10;
     int num_b = 30;
 
-    printf("Address num_a : %p\n", &num_a);
-    printf("Address num_b : %p\n", &num_b);
-
     printf("Before : num_a = %d and num_b = %d\n", num_a, num_b);
-    num_a = inverse_number(num_a, num_b);
+    inverse_number(&num_a, &num_b);
     printf("After : num_a = %d and num_b = %d\n", num_a, num_b);
 
     return 0;
