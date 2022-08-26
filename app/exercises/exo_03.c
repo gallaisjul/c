e0bc39179a6868dd454dae6fc9c7cf02fcb3adb8
Julien G <gallaisjulienlh76@gmail.com>
2022-08-26 20:12:27 +0200

    Display of the two integers

---
+++ app/exercises/exo_03.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * [Affichage du produit de entiers]
+ */
+
+int main(void)
+{
+    int value_01, value_02;
+
+    printf("first value :"); scanf("%d", &value_01);
+    printf("\n");
+
+    printf("second value :"); scanf("%d", &value_02);
+    printf("\n");
+
+    printf("Product of %d and %d = %d", value_01, value_02,value_01 * value_02);
+    printf("\n");
+
+    return 0;
+}
